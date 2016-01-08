9b0305bfa80863c2835c8c9cc6b2c385dce212d5
Craig <craigmiller160>
2016-01-07 22:58:09 -0500

    Created LocalDateConverter to be used by Hibernate to convert Java 8 LocalDate to SQL Date for persistence.

---
+++ src/main/java/io/craigmiller160/orgbuilder/api/v1/model/util/LocalDateConverter.java
@@ -0,0 +1,26 @@
+package io.craigmiller160.orgbuilder.api.v1.model.util;
+
+import javax.persistence.AttributeConverter;
+import java.sql.Date;
+import java.time.LocalDate;
+
+/**
+ * Date conversion utility, used to convert
+ * java.time.LocalDate to java.sql.Date by
+ * persistence frameworks.
+ *
+ * Created by Craig on 1/7/2016.
+ */
+public class LocalDateConverter implements AttributeConverter<LocalDate,Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate localDate) {
+        return localDate == null ? null : Date.valueOf(localDate);
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date date) {
+        return date == null ? null : date.toLocalDate();
+    }
+
+}
