03ffd80501321cd70a055ccba6bc1f66951000a4
Craig <craigmiller160>
2016-01-08 23:09:26 -0500

    Created PersonTest unit test class, primarily for the getAge() method of Person to ensure that it calculates correctly.

---
+++ src/test/java/io/craigmiller160/orgbuilder/api/v1/model/PersonTest.java
@@ -0,0 +1,26 @@
+package io.craigmiller160.orgbuilder.api.v1.model;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * JUnit test case for methods that
+ * do more than just set/get values
+ * in the Person model class.
+ *
+ * Created by Craig on 1/8/2016.
+ */
+public class PersonTest {
+
+    @Test
+    public void testAge(){
+        Person person = new Person();
+        person.setBirthDate(LocalDate.of(1988, 10, 26));
+
+        assertEquals(27, person.getAge());
+    }
+
+}
